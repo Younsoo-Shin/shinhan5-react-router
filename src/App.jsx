71cd8bcc142b4f5eb2a42c80c0c2afcb942caf5c
BeTweak <ys@betweak.com>
2024-11-04 08:13:24 +0900

    no message

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+import mainRouter from './routers/main-router';
+import { RouterProvider } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={mainRouter} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+export default function MainPage() {
+  return (
+    <div>
+        MainPage
+
+        <Link to="/sample">
+            Sample로 이동
+        </Link>
+    </div>
+  )
+}
